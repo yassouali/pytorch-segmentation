b771fdb1cc3fce84537ede4275369976e529ae81
Yassine <ouali.yasine@gmail.com>
2020-10-15 15:03:51 +0200

    corrections

--- models/unet.py
+++ models/unet.py
@@ -8,76 +8,75 @@ from utils.helpers import initialize_weights, set_trainable
 from itertools import chain
 from models import resnet
 
+
+def x2conv(in_channels, out_channels, inner_channels=None):
+    inner_channels = out_channels // 2 if inner_channels is None else inner_channels
+    down_conv = nn.Sequential(
+        nn.Conv2d(in_channels, inner_channels, kernel_size=3, padding=1, bias=False),
+        nn.BatchNorm2d(inner_channels),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(inner_channels, out_channels, kernel_size=3, padding=1, bias=False),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True))
+    return down_conv
+
 class encoder(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(encoder, self).__init__()
-        self.down_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-        )
+        self.down_conv = x2conv(in_channels, out_channels)
         self.pool = nn.MaxPool2d(kernel_size=2, ceil_mode=True)
 
     def forward(self, x):
         x = self.down_conv(x)
-        x_pooled = self.pool(x)
-        return x, x_pooled
+        x = self.pool(x)
+        return x
 
 class decoder(nn.Module):
-    def __init__(self, in_channels, out_channels, interpolate=True):
+    def __init__(self, in_channels, out_channels):
         super(decoder, self).__init__()
-        if interpolate:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+        self.up_conv = x2conv(in_channels, out_channels)
 
-        self.up_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-        )
-
-    def forward(self, x_copy, x, interpolate=False):
+    def forward(self, x_copy, x, interpolate=True):
         x = self.up(x)
 
-        # Padding in case the incomping volumes are of different sizes
-        diffY = x_copy.size()[2] - x.size()[2]
-        diffX = x_copy.size()[3] - x.size()[3]
-        x = F.pad(x, (diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2))
+        if (x.size(2) != x_copy.size(2)) or (x.size(3) != x_copy.size(3)):
+            if interpolate:
+                # Iterpolating instead of padding
+                x = F.interpolate(x, size=(x_copy.size(2), x_copy.size(3)),
+                                mode="bilinear", align_corners=True)
+            else:
+                # Padding in case the incomping volumes are of different sizes
+                diffY = x_copy.size()[2] - x.size()[2]
+                diffX = x_copy.size()[3] - x.size()[3]
+                x = F.pad(x, (diffX // 2, diffX - diffX // 2,
+                                diffY // 2, diffY - diffY // 2))
 
         # Concatenate
         x = torch.cat([x_copy, x], dim=1)
         x = self.up_conv(x)
         return x
 
+
 class UNet(BaseModel):
     def __init__(self, num_classes, in_channels=3, freeze_bn=False, **_):
         super(UNet, self).__init__()
-        self.down1 = encoder(in_channels, 64)
-        self.down2 = encoder(64, 128)
-        self.down3 = encoder(128, 256)
-        self.down4 = encoder(256, 512)
-        self.middle_conv = nn.Sequential(
-            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
-            nn.BatchNorm2d(1024),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
-            nn.BatchNorm2d(1024),
-            nn.ReLU(inplace=True),
-        )
+
+        self.start_conv = x2conv(in_channels, 64)
+        self.down1 = encoder(64, 128)
+        self.down2 = encoder(128, 256)
+        self.down3 = encoder(256, 512)
+        self.down4 = encoder(512, 1024)
+
+        self.middle_conv = x2conv(1024, 1024)
+
         self.up1 = decoder(1024, 512)
         self.up2 = decoder(512, 256)
         self.up3 = decoder(256, 128)
         self.up4 = decoder(128, 64)
         self.final_conv = nn.Conv2d(64, num_classes, kernel_size=1)
         self._initialize_weights()
+
         if freeze_bn:
             self.freeze_bn()
 
@@ -92,15 +91,17 @@ class UNet(BaseModel):
                 module.bias.data.zero_()
 
     def forward(self, x):
-        x1, x = self.down1(x)
-        x2, x = self.down2(x)
-        x3, x = self.down3(x)
-        x4, x = self.down4(x)
-        x = self.middle_conv(x)
+        x1 = self.start_conv(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x = self.middle_conv(self.down4(x4))
+
         x = self.up1(x4, x)
         x = self.up2(x3, x)
         x = self.up3(x2, x)
         x = self.up4(x1, x)
+
         x = self.final_conv(x)
         return x
 
